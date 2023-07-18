2b4430a866341403bb293443a911136614c977ed
ffff-1 <108198336+ffff-1@users.noreply.github.com>
2023-07-18 11:24:26 +0200

    textbox again.

--- GUI.lua
+++ GUI.lua
@@ -74,18 +74,16 @@ local function INIT()
 
         local TEXTBOX = Instance.new"TextBox"
         TEXTBOX.BackgroundColor3 = Color3.new(0.164705, 0.215686, 0.101960)
-        TEXTBOX.BackgroundTransparency = 0.25
+        TEXTBOX.BackgroundTransparency = 0.75
         TEXTBOX.BorderSizePixel = 0
         TEXTBOX.Size = UDim2.fromScale(1,0.03)
         TEXTBOX.AnchorPoint = Vector2.new(0,1)
-        TEXTBOX.Position = UDim2.fromScale(0,1.035)
-        TEXTBOX.TextColor3 = Color3.new(0.19215686274509805, 0.49411764705882355, 0.34901960784313724)
+        TEXTBOX.Position = UDim2.fromScale(0,1.0.6)
+        TEXTBOX.TextColor3 = Color3.new(0.517647, 1, 0)
         TEXTBOX.TextScaled = true
-        TEXTBOX.Text = ""
-        TEXTBOX.PlaceholderText = "Write Command Here"
-        TEXTBOX.ZIndex = 3
         TEXTBOX.TextXAlignment = Enum.TextXAlignment.Left
-        TEXTBOX.TextDirection = Enum.TextDirection.Auto
+        TEXTBOX.TextDirection =Enum.TextDirection.Auto
+        TEXTBOX.Text = ""
         TEXTBOX.Parent = SURFACE
         
         print(SURFACE , TEXTBOX)
